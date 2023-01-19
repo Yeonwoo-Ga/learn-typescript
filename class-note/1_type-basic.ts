04b68c0cb30c261cacbfad12bccfdbaa03c33c6d
Yeonwoo Ga <vjk4874@naver.com>
2023-01-19 21:05:12 +0900

    basic type2

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -9,3 +9,16 @@ let num: number = 10;
 let arr: Array<number> = [1, 2, 3];
 let heroes: Array<string> = ["capt", "thor", "hulk"];
 let items: number[] = [1, 2, 3];
+
+//ts 튜플
+let address1: [string, number] = ["yeon", 11];
+
+//ts 객체
+let obj: object = {};
+let person: { name: string; age: number } = {
+  name: "yeon",
+  age: 1400,
+};
+
+//ts 진위값
+let show: boolean = true;
