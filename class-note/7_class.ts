c3956b4e0381890f59bfb7f27067c4dcb1ffc583
Yeonwoo Ga <vjk4874@naver.com>
2023-01-30 21:31:31 +0900

    generic1

--- class-note/7_class.ts
+++ class-note/7_class.ts
@@ -1,10 +1,28 @@
 class Person3 {
-    name : string;
+    public name : string;
     age : number;
     readonly log : string;
 
     constructor(name : string ,age : number){
         this.name = name;
         this.age = age;
+    }
+}
+
+let haha = new Person3("haha", 28);
+haha.name = 'hehe';
+
+
+class Developer {
+
+    get name() : string{
+        return this.name;
+    }
+
+    set name(newValue : string){
+        if(newValue && newValue.length>5){
+            throw new Error('이름이 너무 깁니다')
+        }
+        this.name = newValue;
     }
 }
--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -1,6 +1,21 @@
-function logText(text){
-    console.log(text)
-    return text;
-}
+// function logText(text){
+//     console.log(text)
+//     return text;
+// }
 
-logText('hi');
+// logText('hi'); //문자열 hi
+// logText(10); // 숫자 10
+// logText(true)// 진위값 true
+
+// function logText<T>(text : T) :T{
+//     console.log(text);
+//     return text;
+// }
+
+function logText(text : string | number)
+
+logText('하이')//호출 시점에 문자열이나 숫자 등 타입을 넘겨 줄 수 있는것이 제네릭
+// logText<string>('헬로')//string타입임을 명시해줄 수 있다.
+
+logText('a');
+logText(10);
